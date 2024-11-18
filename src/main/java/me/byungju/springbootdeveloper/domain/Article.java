218ad010635e48734ad718832ebdd86f33fbdd8a
bj-an <bgan0604@naver.com>
2024-11-18 16:05:54 +0900

    project init

---
+++ src/main/java/me/byungju/springbootdeveloper/domain/Article.java
@@ -0,0 +1,44 @@
+package me.byungju.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    protected Article(){
+
+    }
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+}
