fb8e8bbe85189787b67f6af843b7a4eb7b3dfd82
bj-an <bgan0604@naver.com>
2024-12-18 13:03:24 +0900

    2024-12-18 commit

--- src/main/java/me/byungju/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/me/byungju/springbootdeveloper/config/WebSecurityConfig.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import me.byungju.springbootdeveloper.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -52,5 +56,18 @@ public class WebSecurityConfig {
     }
 
     // 인증 관리자 관련 설정
-    /*작성요망*/
+    public AuthenticationManager authenticationManager (HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService)
+        throws Exception {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService); //사용자 정보 서비스 설정
+        authProvider.setUserDetailsPasswordService(bCryptPasswordEncoder); //사용자 정보 서비스 설정
+        return new ProviderManager(authProvider);
+    }
+
+    //패스워드 인코더로 사용할 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    /*2024.12.18 작성요망*/
 }
